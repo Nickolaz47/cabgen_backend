661171695288a7e5707fd0bfb518ebe07d88be3d
Nicolas da Matta <nicolasaraujo47@gmail.com>
2025-08-28 14:51:40 -0300

    Tests: Added GetUserByUsername handler test.

---
+++ internal/handlers/admin/get_user_by_username_test.go
@@ -0,0 +1,56 @@
+package admin_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/CABGenOrg/cabgen_backend/internal/handlers/admin"
+	"github.com/CABGenOrg/cabgen_backend/internal/models"
+	"github.com/CABGenOrg/cabgen_backend/internal/testutils"
+	testmodels "github.com/CABGenOrg/cabgen_backend/internal/testutils/models"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserByUsername(t *testing.T) {
+	testutils.SetupTestContext()
+	db := testutils.SetupTestRepos()
+
+	mockCountry := testmodels.NewCountry("", "", "", "")
+	db.Create(&mockCountry)
+
+	mockLoginUser := testmodels.NewLoginUser()
+	db.Create(&mockLoginUser)
+
+	t.Run("Success", func(t *testing.T) {
+		c, w := testutils.SetupGinContext(
+			http.MethodGet, "/api/user/nick",
+			"", nil, gin.Params{{Key: "username", Value: "nick"}},
+		)
+
+		admin.GetUserByUsername(c)
+
+		expected := testutils.ToJSON(
+			map[string]models.User{
+				"data": mockLoginUser,
+			},
+		)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.JSONEq(t, expected, w.Body.String())
+	})
+
+	t.Run("User not found", func(t *testing.T) {
+		c, w := testutils.SetupGinContext(
+			http.MethodGet, "/api/user/jao",
+			"", nil, gin.Params{{Key: "username", Value: "jao"}},
+		)
+
+		admin.GetUserByUsername(c)
+
+		expected := `{"error": "User not found."}`
+
+		assert.Equal(t, http.StatusNotFound, w.Code)
+		assert.JSONEq(t, expected, w.Body.String())
+	})
+}
