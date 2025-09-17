c1faceca5b7047b8652b9e60d9ad0f722be5ceec
Nicolas da Matta <nicolasaraujo47@gmail.com>
2025-09-17 11:17:36 -0300

    Tests: Added tests to models package.

---
+++ internal/models/User_test.go
@@ -0,0 +1,45 @@
+package models_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/CABGenOrg/cabgen_backend/internal/models"
+	"github.com/CABGenOrg/cabgen_backend/internal/testutils"
+	testmodels "github.com/CABGenOrg/cabgen_backend/internal/testutils/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToResponse(t *testing.T) {
+	mockUser := testmodels.NewLoginUser()
+	c, _ := testutils.SetupGinContext(http.MethodGet, "/", "", nil, nil)
+
+	mockResponse := mockUser.ToResponse(c)
+	expected := models.UserResponse{
+		ID:          mockUser.ID,
+		Name:        mockUser.Name,
+		Username:    mockUser.Username,
+		Email:       mockUser.Email,
+		CountryCode: mockUser.CountryCode,
+		Country:     mockUser.Country.En,
+		UserRole:    mockUser.UserRole,
+		Interest:    mockUser.Interest,
+		Role:        mockUser.Role,
+		Institution: mockUser.Institution,
+	}
+
+	assert.Equal(t, expected, mockResponse)
+}
+
+func TestToToken(t *testing.T) {
+	mockUser := testmodels.NewLoginUser()
+
+	mockToken := mockUser.ToToken()
+	expected := models.UserToken{
+		ID: mockUser.ID,
+		Username: mockUser.Username,
+		UserRole: mockUser.UserRole,
+	}
+
+	assert.Equal(t, expected, mockToken)
+}
